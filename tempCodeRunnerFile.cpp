65ad4882ba174d9268cfef117e664853b6c8f0ec
Hongcheng Wu <hongcheng1wu@gmail.com>
2020-04-01 23:50:05 -0500

    deleted temp file

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-for(int i = 1; i < numRows;i++){
-        if(i%2 == 0) {
-            for (int j = 1; j < numColumns; j+=2){
-                abs_value = abs(dp[i][j-1] - dp[i][j+1]);
-                dp[i][j] = max(dp[i][j-2], dp[i-1][j-1]) + abs_value;
-            }
-        }
-    }
